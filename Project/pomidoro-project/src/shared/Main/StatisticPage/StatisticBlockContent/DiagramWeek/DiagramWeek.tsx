8f030677a29e4cdeb170212a88082344bff5d38a
Evgeniy Procik <hp-xolodo@yandex.ru>
2024-02-28 20:40:17 +0300

    dev a statistics page-2

--- Project/pomidoro-project/src/shared/Main/StatisticPage/StatisticBlockContent/DiagramWeek/DiagramWeek.tsx
+++ Project/pomidoro-project/src/shared/Main/StatisticPage/StatisticBlockContent/DiagramWeek/DiagramWeek.tsx
@@ -3,16 +3,11 @@ import 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
 
 export function DiagramWeek() {
-  function hoursEarlier(minutes: number) {
-    let time = new Date(new Date().getTime() - (minutes * 60 * 1000));
-    return time;
-  };
   const data = {
-    // labels: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'],
+    labels: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'],
     datasets: [{
         label: 'Work',
-        // data: ['25 м', 50, 75, 100, 200, 0, 0],
-        data: [{id: 'ПН', nested: {value: hoursEarlier(25)}}, {id: 'ВТ', nested: {value: 50}}, {id: 'СР', nested: {value: 25}}, {id: 'ЧТ', nested: {value: 100}}]
+        data: [25, 50, 75, 100, 200, 0, 0],
     }],
   }
 
@@ -26,25 +21,6 @@ export function DiagramWeek() {
               display: false,
             },
           },
-          parsing: {
-            xAxisKey: 'id',
-            yAxisKey: 'nested.value'
-          },
-          scales: {
-            y: {
-              type: 'time',
-              time: {
-                unit: "minute",
-                tooltipFormat: "hh:mm",
-                displayFormats: {
-                  "minute": "hh:mm"
-                }
-              },
-              ticks: {
-                stepSize: 25,
-              }
-            }
-          }
         }}
       />
     </div>
