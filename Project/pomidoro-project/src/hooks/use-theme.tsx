08db652a49c3965b92bb0881128a9a35fc7fcbe8
Evgeniy Procik <hp-xolodo@yandex.ru>
2024-03-08 18:59:11 +0300

    change theme and save localStorage

--- Project/pomidoro-project/src/hooks/use-theme.tsx
+++ Project/pomidoro-project/src/hooks/use-theme.tsx
@@ -1,11 +1,12 @@
 import { useLayoutEffect, useState } from "react";
 
-
 export const useTheme = () => {
-  const [theme, setTheme] = useState('light'); 
+  const localTheme = localStorage.getItem('app-theme');
+  const [theme, setTheme] = useState( localTheme || 'light'); 
 
   useLayoutEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('app-theme', theme);
   }, [theme])
 
   return {theme, setTheme};
--- Project/pomidoro-project/src/shared/Header/Logo/Logo.tsx
+++ Project/pomidoro-project/src/shared/Header/Logo/Logo.tsx
@@ -1,13 +1,14 @@
 import './logo.css';
 import { Tomato } from '../../../assets';
+import { Link } from 'react-router-dom';
 
 export function Logo() {
   return (
     <div className='headerLogo'>
-      <a href="/" className='logo'>
+      <Link to='/' className='logo'>
         <Tomato />
         <span className='headerLogoText'>pomodoro_box</span>
-      </a>
+      </Link>
     </div>
   );
 }
