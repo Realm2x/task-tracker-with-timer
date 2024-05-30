dec273327fffd1481a4096d04622cc0abebf22fc
Evgeniy Procik <hp-xolodo@yandex.ru>
2024-05-30 19:22:46 +0300

    add folder docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/task-tracker-with-timer",
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     svgr({
