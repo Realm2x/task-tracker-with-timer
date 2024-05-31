462ea23ef715a8a758ff6fca6cd0e2661908e1b2
Evgeniy Procik <hp-xolodo@yandex.ru>
2024-05-31 17:15:36 +0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "./",
+  base: "/realm2x.github.io/task-tracker-with-timer/",
   build: {
     outDir: 'dist',
     emptyOutDir: true,
